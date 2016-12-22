64b20a461de08f79f8f5def11b556f093f563fb8
Sandbox Sync <support@getsandbox.com>
2016-12-22 03:56:13 +0000

    Changes

--- stubs/main.js
+++ stubs/main.js
@@ -2716,20 +2716,12 @@ Sandbox.define('/ping','GET', function(req, res){
     });
 })
 
-Sandbox.define('/MockService','POST', function(req, res) {
-    // Check the request, make sure it is a compatible type
-    if (!req.is('application/json')) {
-        return res.send(400, 'Invalid content type, expected application/json');
-    }
+Sandbox.define('/MockService','POST', function(req, res){
+    res.type(Sandbox.config.defaultResponseType);
     
-    // Set the type of response, sets the content type.
-    res.type('application/json');
-    
-    // Set the status code of the response.
     res.status(202);
     
-    // Send the response body.
     res.json({
-        "status": "ok"
+        "status": "Accepted"
     });
 })
