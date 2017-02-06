5f0fcc1f2519b5b0d68362efa07095d2ac5cebc6
Sandbox Sync <support@getsandbox.com>
2017-02-06 22:26:21 +0000

    Changes

--- stubs/main.js
+++ stubs/main.js
@@ -2936,7 +2936,7 @@ Sandbox.define('/MockService','POST', function(req, res){
 Sandbox.define('/MockService','PUT', function(req, res){
     res.type(Sandbox.config.defaultResponseType);
     
-    res.status(202);
+    res.status(Sandbox.config.http_accepted_status_code);
     
     res.json({
         "status": "Accepted"
