b2a07e1de43c6861277301fadde74b5f6923d3a6
Sandbox Sync <support@getsandbox.com>
2017-02-22 03:30:09 +0000

    Changes

--- stubs/main.js
+++ stubs/main.js
@@ -2943,3 +2943,21 @@ Sandbox.define('/MockService','PUT', function(req, res){
         "status": "Accepted"
     });
 })
+
+Sandbox.define('/','POST', function(req, res) {
+    // Check the request, make sure it is a compatible type
+    if (!req.is('application/json')) {
+        return res.send(400, 'Invalid content type, expected application/json');
+    }
+    
+    // Set the type of response, sets the content type.
+    res.type('application/json');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.json({
+        "status": "ok"
+    });
+})
