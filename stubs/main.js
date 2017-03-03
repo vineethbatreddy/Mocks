a0e607a2e4f42a18b8c82cc8ad0de74cbf02174b
Sandbox Sync <support@getsandbox.com>
2017-03-03 03:07:19 +0000

    Changes

--- stubs/main.js
+++ stubs/main.js
@@ -2944,3 +2944,37 @@ Sandbox.define('/MockService','PUT', function(req, res){
     });
 })
 
+
+
+Sandbox.define('/numberManagement/fnn','GET', function(req, res) {
+    // Check the request, make sure it is a compatible type
+    if (!req.is('application/json')) {
+        return res.send(400, 'Invalid content type, expected application/json');
+    }
+    
+    // Set the type of response, sets the content type.
+    res.type('application/json');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.json({
+        "data": [{
+            "fnn": "03 67672091",
+            "status": "valid"
+        }, {
+            "fnn": "03 67683091",
+            "status": "valid"
+        }, {
+            "fnn": "03 67672191",
+            "status": "valid"
+        }, {
+            "fnn": "03 67672291",
+            "status": "valid"
+        }, {
+            "fnn": "03 90002091",
+            "status": "valid"
+        }]
+    });
+})
