4ada57cd0b27edf88462a471a8afd5f6050d48d4
Sandbox Sync <support@getsandbox.com>
2017-03-09 01:07:09 +0000

    Changes

--- stubs/main.js
+++ stubs/main.js
@@ -2984,3 +2984,19 @@ Sandbox.soap('/QueryAppointment','', function(req, res) {
     // Send the response body.
     res.render('');
 })
+
+Sandbox.soap('/RescheduleAppointment','', function(req, res) {
+    // Check the request, make sure it is a compatible type, covers both SOAP 1.1 and 1.2
+    if (!req.is('text/xml') && !req.is('application/xml') && !req.is('application/soap')) {
+        return res.send(400, 'Invalid content type, expected application/soap+xml');
+    }
+    
+    // Set the type of response, sets the content type.
+    res.type('application/soap+xml');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.render('');
+})
