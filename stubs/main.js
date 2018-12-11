759c9c07c6a220b147e1cb2058cbe2d29ada74b8
Sandbox Sync <support@getsandbox.com>
2018-12-11 22:48:36 +0000

    Changes

--- stubs/main.js
+++ stubs/main.js
@@ -2919,7 +2919,7 @@ Sandbox.define('/ping','GET', function(req, res){
     });
 })
 
-Sandbox.define('/MockService','POST', function(req, res){
+Sandbox.define('/destroy/MockService', 'POST', function(req, res){
     res.type(Sandbox.config.default_response_type);
     
     res.status(Sandbox.config.http_accepted_status_code);
