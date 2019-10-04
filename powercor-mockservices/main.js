ad1a72a447c260a8bbbbeed01dc5cf2f2dac0112
Sandbox Sync <support@getsandbox.com>
2019-10-04 05:31:58 +0000

    Changes

--- powercor-mockservices/main.js
+++ powercor-mockservices/main.js
@@ -304,3 +304,21 @@ Sandbox.define('/DERRSave','POST', function(req, res){
     
     res.send(responseXML);
 })
+
+Sandbox.define('/DERRQuery','POST', function(req, res) {
+    // Check the request, make sure it is a compatible type
+    if (!req.is('application/xml')) {
+        return res.send(400, 'Invalid content type, expected application/xml');
+    }
+    
+    // Set the type of response, sets the content type.
+    res.type('application/json');
+    
+    // Set the status code of the response.
+    res.status(200);
+    
+    // Send the response body.
+    res.json({
+        "status": "ok"
+    });
+})
